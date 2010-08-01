511d33c89072ffca6549f303f8e8c4c7ec11ff44
Patricio Paez <pp@pp.com.mx>
2010-08-01 06:07:59 -0500

    Ordenar usando key = lambda

--- biblio.py
+++ biblio.py
@@ -3,20 +3,16 @@
 import yaml
 f=open('biblio.yaml')
 obras=yaml.load(f)
-f.close
-a=[]
-for obra in obras:
-  a.append(obra['Autor'])
-b=a[:]
-b.sort()
-c=[]
-for siguiente in b:
-  c.append(a.index(siguiente))
+f.close()
+
+#Indicar una llave para ordenar
+obras.sort( key = lambda elemento: elemento[ 'Autor' ] )
+
 print('<table width="95%" cellspacing="0" cellpadding="5" style="margin:2%; border:thin solid #000000; text-align:center;">')
 print('<tr style="background-color: #aaaaaa"><th>Título</th><th>Autor</th><th>Año</th><th>Editorial</th><th>Reseña</th></tr>')
-for siguiente in c:
-  obra=obras[siguiente]
-  if(c.index(siguiente)%2 == 0):
+
+for obra in obras:
+  if( obras.index( obra )%2 == 0):
     print('<tr style="background-color:#eeeeee ;">')
   else:
     print('<tr style="background-color:#ffffff ;">')
