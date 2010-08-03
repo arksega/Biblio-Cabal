154600b4d5d73e42e1d19f8ed6855633d6cf8027
Ville1ero <Ville1ero@gmail.com>
2010-08-02 20:34:18 -0500

    Salida dirigida a archivo

--- biblio.py
+++ biblio.py
@@ -5,6 +5,7 @@ f=open('biblio.yaml')
 obras=yaml.load(f)
 f.close
 a=[]
+salida=open('tabla.html','w')
 for obra in obras:
   a.append(obra['Autor'])
 b=a[:]
@@ -12,15 +13,15 @@ b.sort()
 c=[]
 for siguiente in b:
   c.append(a.index(siguiente))
-print('<table width="95%" cellspacing="0" cellpadding="5" style="margin:2%; border:thin solid #000000; text-align:center;">')
-print('<tr style="background-color: #aaaaaa"><th>Título</th><th>Autor</th><th>Año</th><th>Editorial</th><th>Reseña</th></tr>')
+print('<table width="95%" cellspacing="0" cellpadding="5" style="margin:2%; border:thin solid #000000; text-align:center;">',file=salida)
+print('<tr style="background-color: #aaaaaa"><th>Título</th><th>Autor</th><th>Año</th><th>Editorial</th><th>Reseña</th></tr>',file=salida)
 for siguiente in c:
   obra=obras[siguiente]
   if(c.index(siguiente)%2 == 0):
-    print('<tr style="background-color:#eeeeee ;">')
+    print('<tr style="background-color:#eeeeee ;">',file=salida)
   else:
-    print('<tr style="background-color:#ffffff ;">')
+    print('<tr style="background-color:#ffffff ;">',file=salida)
 
-  print( '<td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td>'.format( obra[ 'Título' ], obra[ 'Autor' ], obra[ 'Año' ],obra[ 'Editorial' ] ,obra['Reseña']) )
-  print('</tr>')
-print('</table>') 
+  print( '<td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td>'.format( obra[ 'Título' ], obra[ 'Autor' ], obra[ 'Año' ],obra[ 'Editorial' ] ,obra['Reseña']),file=salida )
+  print('</tr>',file=salida)
+print('</table>',file=salida) 
