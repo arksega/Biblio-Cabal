666308f915a813ba7cec6b0110c7be894330f458
Renich Bon Ciric <renich@woralelandia.com>
2010-08-10 02:17:34 -0500

    Inclusión de "Templates"
    He agregado la posibilidad de definir templates. Ahora, solo tienes que
    basarte en el template actual para poder generar diferentes versiones de
    tabla.
    
    También cambié a usar tablas completas para cada libro (y, así, poder
    listar todos sus datos).
    
    En conclusión, éste sistema puede ser usado para generar más que solo la
    tabla XHTML, sino, otros layouts como uso de divs, PDFs, XML, etc.
    
    Falta reescribir para:
    
    - no tener que reescribir el código para usar otro formato
    - calcular las extensiones de los archivos según el template usado
    - inclusión dinámica de módulos (pdf, xml, etc)

--- biblio.py
+++ biblio.py
@@ -1,26 +1,36 @@
-# -*- coding: utf-8 -*-
-
 import yaml
-f = open( 'biblio.yaml' )
-obras = yaml.load( f )
 
-def genera( llave, nombre_archivo ):
-  salida = open( nombre_archivo, 'w' )
+# abrir base de datos (yaml)
+f = open('biblio.yaml')
 
-  #Indicar una llave para ordenar
-  obras.sort( key = lambda elemento: elemento[ llave ] )
-  print( '<table width="95%" cellspacing="0" cellpadding="5" style="margin:2%; border:thin solid #000000; text-align:center;">', file = salida )
-  print( '<tr style="background-color: #aaaaaa"><th>Título</th><th>Autor</th><th>Año</th><th>Editorial</th><th>Reseña</th></tr>', file = salida)  
-  
-  for obra in obras:
-    if( obras.index( obra )%2 == 0 ):
-      print( '<tr style="background-color:#eeeeee ;">', file = salida )
-    else:
-      print( '<tr style="background-color: #ffffff;">', file = salida )  
-    print( '<td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td>'.format( obra[ 'Título' ], obra[ 'Autor' ], obra[ 'Año' ],obra[ 'Editorial' ] ,obra[ 'Reseña' ] ), file = salida )
-    print( '</tr>', file = salida )
-  print( '</table>', file = salida )
+# importarla a un objeto
+obras = yaml.load(f)
 
+# generar archivos según el tipo de template
+def genera(llave, nombre_archivo):
+    
+    # abrir archivo a escribir
+    salida = open(nombre_archivo, 'w')
+
+    # indicar una llave para ordenar
+    obras.sort(key = lambda elemento: elemento[llave])
+
+    # abrir template correspondiente
+    template = open('templates/tabla-xhtml.tpl', 'r').read()
+    
+    for obra in obras:
+        # substtuir datos
+        print(template.format( obra['Título'], 
+                               obra['Autor'], 
+                               obra['Año'], 
+                               obra['ISBN'], 
+                               obra['Idioma'] , 
+                               obra['Formato'], 
+                               obra['Editorial'], 
+                               obra['Fuente'], 
+                               obra['Tags'], 
+                               obra['Reseña']),
+              file = salida)
+    
+    
 genera( 'Autor', 'autor.html' )
-genera( 'Título', 'titulo.html' )
-genera( 'Año', 'anio.html' )
